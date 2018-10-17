52e2b037a66bafcaca3e304f7a79a2fadb58ec8f
Arron Zhu <1274294199@qq.com>
2018-10-17 09:39:00 +0800

    微信扫码样式

--- src/pages/wechat/QRcode.vue
+++ src/pages/wechat/QRcode.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
+  <div class="QRdiv-wrapper">
     <div class="QRdiv">
       <img :src="QRcode">
+      <div class="img-footer">
+      <div class="first-row">使用手机微信扫码登录</div>
+      <div class="second-row">网页版微信需要配合手机使用</div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,8 +27,23 @@
 </script>
 
 <style scoped>
+  .QRdiv-wrapper{
+    margin: 0 auto;
+  }
   .QRdiv{
+    width: fit-content;
     text-align: center;
-    margin-top: 10%;
+    margin:8% auto;
+    padding: 2% 2% 5% 2%;
+    background-color: white;
+  }
+  .img-footer .first-row{
+    font-size: 18px;
+    color: #333333;
+    margin: 1vh 0;
+  }
+  .img-footer .second-row{
+    color: #9ea7b4;
+    font-size: 14px;
   }
 </style>
